46338c3151503800ac44e65fac85c579a2ff1f62
Daniel White <24dw3359@barton.ac.uk>
2025-09-05 14:04:39 +0100

    added temp code to test textrank

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Computer_Science_A_Level_NEA;
+using MimeKit;
 using System.Data.SQLite;
 // Copyright 2025 Daniel Ian White
 
@@ -255,7 +256,6 @@ namespace NEA_protoype
                             Console.Write("Enter EmailAddress Password (Please note it is not hidden): ");
                             // TODO - validate password 
                             string EmailPassword = Console.ReadLine();
-
                             new SQLiteCommand("INSERT INTO Users(EmailAddress,Password,Mailserver,Account)" +
                                               "VALUES " +
                                               $"('{EmailAddress}','{Encryption.Encrypt(EmailPassword)}','{MailServer}','{accountName}');", connection).ExecuteNonQuery();
@@ -426,16 +426,41 @@ namespace NEA_protoype
                 connection.Close();
             }
             ConsoleInteraction.CheckConsoleExistance();
-            AccountsMenu();
+            // AccountsMenu();
+
+            // testing text rank delete after finishing
+            char[] Body = "Dear All\r\n\r\nTrip to Leonardo event\r\nTuesday 16th September\r\n\r\nWe (CANSAT teams) have been invited to attend a special event at Leonard Southampton to celebrate their new space technology. \r\n\r\nMore details below on the technology.\r\nDate: 16th September \r\nI will take you there and back in minibus.\r\n\r\nWill involve some talks and a tour and lunch\r\n\r\nWe have been given 12 tickets. So it is first come first serves.\r\n\r\nIf you would like to come on this trip then please email ME (not Leonardo!) letting me know that you want to come and any dietary requirements AS SOON AS POSSIBLE. \r\nThey want to know by 1st August ideally, but just let me know as soon as you can.\r\n\r\nALSO Leonardo have asked us to not tell anyone about this event till afterwards!!\r\nAny questions let me know.".ToCharArray();
+            List<string> FilteredWords = new List<string>();
+            string[] WordsToFilter = { "the", "\n", "\r", "is", "and", "a" };
+            string Temp = "";
+            string text = "";
+             
+                foreach (string word in WordsToFilter)
+                {
+                    for (int i = 0; i < Body.Length - word.Length; i++)
+                    {
+                        Temp += "";
+                        for (int j = 0; j < word.Length; j++)
+                        {
+                            Temp += Body[i + j];
+                        }
+                        if (Temp == word)
+                        {
+                            for (int k = 0; k < word.Length; k++)
+                            {
+                                Body[i + k] = ' ';
+                            }
+                        }
+                    }
+                }
+                foreach (char character in Body)
+                { text += character; }
 
 
 
 
 
-
-
-
-            Console.ReadLine();
+                Console.ReadLine();
 
         }
 
