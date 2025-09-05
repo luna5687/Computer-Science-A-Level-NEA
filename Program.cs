cd70ed8f4eedbfe10d671b1925e5b32f803629d2
Daniel White <80250666+luna5687@users.noreply.github.com>
2025-09-05 14:42:58 +0000

    updated program.cs

--- Program.cs
+++ Program.cs
@@ -417,10 +417,10 @@ namespace NEA_protoype
                 connection.Close();
             }
             ConsoleInteraction.CheckConsoleExistance();
-            AccountsMenu();
+            //AccountsMenu();
 
             // testing text rank delete after finishing 
-            /*
+            
             char[] Body = "Dear All\r\n\r\nTrip to Leonardo event\r\nTuesday 16th September\r\n\r\nWe (CANSAT teams) have been invited to attend a special event at Leonard Southampton to celebrate their new space technology. \r\n\r\nMore details below on the technology.\r\nDate: 16th September \r\nI will take you there and back in minibus.\r\n\r\nWill involve some talks and a tour and lunch\r\n\r\nWe have been given 12 tickets. So it is first come first serves.\r\n\r\nIf you would like to come on this trip then please email ME (not Leonardo!) letting me know that you want to come and any dietary requirements AS SOON AS POSSIBLE. \r\nThey want to know by 1st August ideally, but just let me know as soon as you can.\r\n\r\nALSO Leonardo have asked us to not tell anyone about this event till afterwards!!\r\nAny questions let me know.".ToCharArray();
             List<string> FilteredWords = new List<string>();
             string[] WordsToFilter = { "the", "\n", "\r", "is", "and", "a" };
@@ -447,8 +447,8 @@ namespace NEA_protoype
                 }
                 foreach (char character in Body)
                 { text += character; }
-                
-               */
+                Console.WriteLine(text);
+               
                 Console.ReadLine();
 
         }
