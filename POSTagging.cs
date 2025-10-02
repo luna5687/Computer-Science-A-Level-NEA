cc7001d217caaa4cbe1a7b772abe074c6152f40a
Daniel White <danielwhite1722@gmail.com>
2025-10-02 21:51:20 +0100

    added lexicon and used it ot pos tag

--- POSTagging.cs
+++ POSTagging.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using MimeKit.Cryptography;
+using Org.BouncyCastle.Tls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace Computer_Science_A_Level_NEA
 {
     static class POSTagging
@@ -12,13 +14,35 @@ namespace Computer_Science_A_Level_NEA
         {
             public string value;
             public string wordType;
-        }
-        static public string[] Determiners = { "a", "an", "the", "this", "that", "these", "those", "my", "your", "his", "her", "it's", "our", "their" };
-        // Det = determiners, N = noun, V = verb, Adv = adverb,adj = adjactive
+        // Det = determiners, N = noun, V = verb, Adv = adverb,Adj = adjactive
         // could add a list of common adjectives e.g new, imporant etc
-        // check to see if a word has been tagged later if it has been tagged ealier on 
+        // follow stepin in paper
+        static private List<word> Lexicon;
+        static public void FillLexicon()
+        {
+            Lexicon = new List<word>();
+            string Words = File.ReadAllText("Lexicon.txt");
+            string[] strings = Words.Split("\n");
+            for (int i = 0; i < strings.Length-1; i++)
+            {
+                strings[i] = strings[i].Substring(0, strings[i].Length - 1);    
+            }
+            word temp;
+            foreach (string s in strings)
+            {
+                if (s != "")
+                {
+                    temp = new word();
+                    temp.value = s.Substring(0, s.IndexOf(','));
+                    temp.wordType = s.Substring(s.IndexOf(",") + 1);
+                    Lexicon.Add(temp);
+                }
+            }
+        }
         static public void POStagging(string input) // tessting POS
         {
+            FillLexicon();
+
             string[] WordsToFilter = { "\n", "\r" };
             char[] Body = input.ToCharArray();
             string Temp = "";
@@ -57,10 +81,14 @@ namespace Computer_Science_A_Level_NEA
                         {
                             TempWord = new word();
                             TempWord.value = word2;
-                            if (Determiners.Contains(word2))
+                            foreach (word w in Lexicon)
                             {
-                                TempWord.wordType = "Det";
+                                if (TempWord.value == w.value)
+                                {
+                                    TempWord.wordType = w.wordType;
+                                }
                             }
+                            
                             TempList.Add(TempWord);
                         }
                     }
@@ -68,17 +96,30 @@ namespace Computer_Science_A_Level_NEA
                     TempList = new List<word>();
                 }
             }
+            for (int i = 0; i < SplitSentences.Count; i++)
+            {
+                if (SplitSentences[i].Count == 0)
+                {
+                    SplitSentences.RemoveAt(i);
+                }
+
+            }
+
+
+            /*
             Console.WriteLine();
+            word temp;
             for (int i = 0; i < SplitSentences.Count; i++)
             {
                 for (int j = 0; j < SplitSentences[i].Count; j++)
                 {
-                    word temp = SplitSentences[i][j];
+                     temp = SplitSentences[i][j];
                     if (j != 0)
                     {
                         if (SplitSentences[i][j-1].wordType == "Det")
                         {
                             temp.wordType = "N";
+                            temp.Certinity = 1;
                         }
                         if ((int)SplitSentences[i][j].value[0] >= 65 && (int)SplitSentences[i][j].value[0] <= 90)
                         {
@@ -88,6 +129,36 @@ namespace Computer_Science_A_Level_NEA
                     SplitSentences[i][j] = temp;
                 }
             }
+            int count1 = 0;
+            int count2 = 0;
+            
+            /*
+            for (int i = 0;i < SplitSentences.Count;i++)
+            {
+                for (int j = 0; j < SplitSentences[i].Count; j++)
+                {
+                    count1 = 0;
+                    count2 = 0;
+                    while (SplitSentences[i][j].wordType == null && count1 < SplitSentences.Count)
+                    {
+                        
+                        if (SplitSentences[count1][count2].wordType != null && SplitSentences[count1][count2].value == SplitSentences[i][j].value)
+                        {
+                            temp = SplitSentences[i][j];
+                            temp.wordType = SplitSentences[count1][count2].wordType;
+                            temp.Certinity = 2;
+                            SplitSentences[i][j] = temp;
+                        }
+                        count2++;
+                        if (count2 >= SplitSentences[count1].Count)
+                        {
+                            count1++;
+                            count2 = 0;
+                        }
+                    }
+                }
+            }
+            */
             foreach (List<word> sentences in SplitSentences)
             {
                 foreach (word sent in sentences)
@@ -96,6 +167,7 @@ namespace Computer_Science_A_Level_NEA
                 }
                 Console.WriteLine();
             }
+            
         }
     }
 }
